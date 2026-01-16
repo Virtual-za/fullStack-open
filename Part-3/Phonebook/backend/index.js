eac1696dbece635048182061e81e78c50448e04f
James Broadbent <jameshambr@gmail.com>
2026-01-16 09:02:39 +0200

    Changed Folder name

--- Part-3/Phonebook/backend/index.js
+++ Part-3/Phonebook/backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-
+const cors = require('cors')
+app.use(cors())
 let persons = [
     { 
       "id": "1",
@@ -98,7 +99,7 @@ const person = {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })
